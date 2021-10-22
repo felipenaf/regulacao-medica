ab616ad811bc9725c8162eddf4ed80c881941baf
Felipe <felipe.ferreira0192@gmail.com>
2021-10-22 16:26:57 -0300

    centralização de css

--- resources/views/encaminhamento/cadastro.blade.php
+++ resources/views/encaminhamento/cadastro.blade.php
@@ -10,69 +10,22 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            .invalid-feedback {
-                color: red;
-            }
-        </style>
+        <link rel="stylesheet" href="{{ \Illuminate\Support\Facades\URL::asset('css/style.css') }}">
     </head>
     <body>
-        <div class="flex-center position-ref ">
+        <div class="m-l-md position-ref ">
             <div class="">
-                <h2 class="m-b-md">
-                    Cadastro de Encaminhamento
-                </h4>
+                <div>
+                    <h2>
+                        Cadastro de Encaminhamento
+                    </h2>
+                </div>
+
+                <div class="">
+                    <a href="{{url('/encaminhamento')}}">
+                        Voltar
+                    </a>
+                </div>
 
                 <div>
                     <form action="{{ route('registrar_encaminhamento') }}" method="POST">
